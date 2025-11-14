db9144323515200fd94f548ecee2e9c162141caa
Gabriel <jhleongg@gmail.com>
2025-11-14 12:00:04 +0800

    Added echo

--- src/main/java/Vulpes.java
+++ src/main/java/Vulpes.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Vulpes {
+    public static void hello() {
+        System.out.println("Canis Lupus? Vulpes vulpes!\n");
+        System.out.println("What'd the doctor say?\n");
+    }
+
+    public static void echo() {
+        Scanner scanner = new Scanner(System.in);
+        String echo = scanner.nextLine();
+        while (!echo.equals("bye")) {
+            System.out.println(echo + "\n");
+            echo = scanner.nextLine();
+        }
+        bye();
+    }
+
+    public static void bye() {
+        System.out.println("*whistles, clicks tongue* (Bye!)");
+    }
+
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n");
-        System.out.println("Hello! I'm Vulpes");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________\n");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________\n");
+        hello();
+        echo();
     }
 }
