d34da675733634851735ac29393aed49474cbf5c
Gabriel <jhleongg@gmail.com>
2025-11-22 16:44:00 +0800

    Revert "Amended Vulpes, Storage, Tasklist, added command for archives - mistake made forgetting that run cannot be modified"
    This reverts commit 8f7eae4a3036cbaae1bf406a3f6f8c8adcba7688.

--- src/main/java/vulpes/Vulpes.java
+++ src/main/java/vulpes/Vulpes.java
@@ -1,5 +1,3 @@
-package vulpes;
-
 import vulpes.command.Command;
 import vulpes.exception.VulpesException;
 import vulpes.parser.Parser;
@@ -7,45 +5,41 @@ import vulpes.storage.Storage;
 import vulpes.tasklist.TaskList;
 import vulpes.ui.Ui;
 
-import java.nio.file.Path;
-
 
 /**
  * // <a href="https://letterboxd.com/film/fantastic-mr-fox/">...</a>
  * Credits to Google and Google Gemini, W3Schools, StackOverflow and to LeeJiaHao's Repo for some ideas (L0/1/2 only)
- * A028761[8]M for the individual features : B-ViewSchedules / C-Archive / B-FixedDurationTasks
  */
 
 public class Vulpes {
     //TODO: TextUiTesting if time allows
 
-    private Storage list, archived; // OOP principles, 1 for each
+    /**
+     * Allocating instances of classes specified in 'run' method
+     */
+    private Storage storage;
     private Ui ui;
-    private TaskList listTasks, archivedTasks; // OOP principles, 1 for each
+    private TaskList tasks;
 
     /**
      * Instantiation of classes specified in 'run' method
-     * @param listPath The file path at which the list will be saved/loaded from the user's local directory
-     * @param archivedPath The file path at which the archives will be saved/loaded from the user's local directory
+     * @param filePath The file path at which the list will be saved/loaded from the user's local directory
      */
-    public Vulpes(String listPath, String archivedPath) {
+    public Vulpes(String filePath) {
         ui = new Ui();
-        list = new Storage(listPath);
-        archived = new Storage(archivedPath);
+        storage = new Storage(filePath);
         try {
-            listTasks = new TaskList(list.load(Path.of(listPath)).getAllTasks());
-            archivedTasks = new TaskList(archived.load(Path.of(archivedPath)).getAllTasks());
+            tasks = new TaskList(storage.load().getAllTasks());
         } catch (VulpesException e) {
             ui.showError("...");
-            listTasks = new TaskList(); // start with empty list
-            archivedTasks = new TaskList(); // start with empty list
+            tasks = new TaskList(); // start with empty list
         }
     }
 
     /**
      * Specified method, untouched and original
      */
-    public void run() { // assume I cannot change this; I personally would rewrite this to better implement archives
+    public void run() { // assume I cannot change this
         ui.showWelcome();
         boolean isExit = false;
         while (!isExit) {
@@ -64,9 +58,9 @@ public class Vulpes {
     }
 
     /**
-     * Main function now pulls from two paths - list and archived
+     * Self-explanatory
      */
     public static void main(String[] args) {
-        new Vulpes("data/Vulpes.txt", "data/Archived.txt").run();
+        new Vulpes("data/Vulpes.txt").run();
     }
 }
