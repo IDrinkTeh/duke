09380ee6ea9070d10151b582b9a1d329bb28f777
Gabriel <jhleongg@gmail.com>
2025-11-23 23:05:33 +0800

    Amended flavour

--- src/main/java/vulpes/command/ListCommand.java
+++ src/main/java/vulpes/command/ListCommand.java
@@ -20,7 +20,7 @@ public class ListCommand extends Command { // to list
      */
     @Override
     public void execute(TaskList listTasks, Ui ui, Storage list) {
-        ui.showMessage("Synchronize your clocks. The time is now 9:45 MissingParametersException.m. Here, put these bandit hats on."); // flavour
+        ui.showMessage("Synchronize your clocks. The time is now 9:45 am. Here, put these bandit hats on."); // flavour
 
         if (listTasks.isEmpty("")) { // check for empty list
             ui.showMessage("The list is empty. There are no targets at the moment.");
